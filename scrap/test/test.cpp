c8db5a84a003988c4dfc5a63c85b77ce3c2f21c4
Thomas Holland <101332229+0thomasholland@users.noreply.github.com>
2026-06-28 12:19:00 +0100

    First play with cpp

---
+++ scrap/test/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+  cout << "TEST TEST TEST!!!" << endl;
+  long double x = 10418121231231323123;
+  cout << "Sine value of x=2.3 : " << sin(x) << endl;
+  double y = 10418121231231323123;
+  cout << "Sine value of x=2.3 : " << sin(y) << endl;
+  float z = 10418121231231323123;
+  cout << "Sine value of x=2.3 : " << sin(z) << endl;
+  return 0;
+}
